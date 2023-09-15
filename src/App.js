829a6e68eac8a8190745b5700b75c25ae6fab7c2
fahmidanial <naloniel2000@gmail.com>
2023-09-15 15:21:24 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Header from './components/Header';
+import Post from './components/Post';
+import SelectedItem from './components/SelectedItem';
+import Barry from './otters/otter1.jpg';
+import Robin from './otters/otter2.jpg';
+import Maurice from './otters/otter3.jpg';
+import Lesley from './otters/otter4.jpg';
+import Barbara from './otters/otter5.jpg';
+
+const ottersArray = [
+  { image: Barry, name: 'Barry', id: 1 },
+  { image: Robin, name: 'Robin', id: 2 },
+  { image: Maurice, name: 'Maurice', id: 3 },
+  { image: Lesley, name: 'Lesley', id: 4 },
+  { image: Barbara, name: 'Barbara', id: 5 },
+];
 
 function App() {
+  const [selectedPostName, setSelectedPostName] = useState('Barry');
+  
+  const selectedPost = ottersArray.find(otter => otter.name === selectedPostName);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div className='app-content'>
+        <ul className='post-list'>
+          {ottersArray.map((post) => (
+            <Post
+              key={post.id}
+              image={post.image}
+              name={post.name}
+              setSelectedPostName={setSelectedPostName}
+            />
+          ))}
+        </ul>
+        <SelectedItem
+          image={selectedPost.image}
+          name={selectedPost.name}
+        />
+      </div>
     </div>
   );
 }
